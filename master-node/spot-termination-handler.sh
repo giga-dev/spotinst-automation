c27c2e078c99ffb59ca3bfc41ef1a50595817ca2
yohanakh <yohana.khoury@gmail.com>
2019-06-27 20:26:07 +0300

    log output to file

--- master-node/spot-termination-handler.sh
+++ master-node/spot-termination-handler.sh
@@ -5,7 +5,7 @@ function logger {
 
 while true; do
     if [[ -z $(curl -Is http://169.254.169.254/latest/meta-data/spot/instance-action | head -1 | grep 404 | cut -d ' ' -f 2) ]]; then
-        echo "Running shutdown hook."
+        logger "Running shutdown hook."
         sleep 1m
         supervisorctl stop all
         docker stop $(docker ps -aq)
