f4e13e93bf7edece9d4b0c977ed16b950abff467
Yohana Khoury <yohana@yohanakhoury.com>
2019-07-24 19:29:59 +0300

    use supervisor version 4.0.4

--- supervisor/install.sh
+++ supervisor/install.sh
@@ -3,7 +3,7 @@ DIRNAME=$(dirname ${BASH_SOURCE[0]})
 
 if [ ! -e "/etc/supervisord/" ]; then
 	yum install python-pip -y
-	pip install supervisor
+	pip install supervisor==4.0.4
 
 	mkdir -p /etc/supervisord.d
 	cp ${DIRNAME}/supervisord.conf /etc/supervisord.conf
@@ -11,8 +11,8 @@ if [ ! -e "/etc/supervisord/" ]; then
 
 	cp ${DIRNAME}/supervisord.service /usr/lib/systemd/system/supervisord.service
 
-    groupadd supervisor
-    usermod -a ec2-user -G supervisor
+	groupadd supervisor
+	usermod -a ec2-user -G supervisor
 	systemctl start supervisord
 	systemctl enable supervisord
 fi
