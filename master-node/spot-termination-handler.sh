efcdd8b720a605e8fd41e582899b9331c762bd38
yohanakh <yohana@yohanakhoury.com>
2020-04-02 21:44:21 +0000

    better prints for shutdown handler + restart services after 20m incase it's a false alarm

--- master-node/spot-termination-handler.sh
+++ master-node/spot-termination-handler.sh
@@ -5,16 +5,22 @@ function logger {
 }
 
 logger "Starting monitoring for spot termination"
+aws configure set region us-east-2
 
 while true; do
-    if [[ -z $(curl -Is http://169.254.169.254/latest/meta-data/spot/instance-action | head -1 | grep 404 | cut -d ' ' -f 2) ]]; then
+    res=$(curl -Is http://169.254.169.254/latest/meta-data/spot/instance-action)
+    if [[ -z $(echo $res | head -1 | grep 404 | cut -d ' ' -f 2) ]]; then
         logger "Detected shutdown event for the instance, running shutdown hook"
-        logger "Message was: $(curl -Is http://169.254.169.254/latest/meta-data/spot/instance-action)"
+        logger "Message was: $res"
+	aws sns publish --message="Detected shutdown event for the instance, running shutdown hook. Message was: $res" --topic-arn "arn:aws:sns:us-east-2:573366771204:Spotinst-instances" --subject "Groot is going down $(date +'%Y-%m-%d %H:%M:%S')"
         sleep 1m
         supervisorctl stop all
         docker stop $(docker ps -aq)
         logger "Shutdown was completed"
-        break
+        sleep 20m
+        logger "Apparently it was a false alarm, restarting all services"
+        supervisorctl start all
+        sleep 1m
     else
         sleep 10s
     fi
